c72b0ad2e61600876047224f906ed5ef50397dce
aclaudel <arnaud.claudel@codurance.com>
2019-11-15 17:47:26 +0000

    add special case for sulfuras

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,12 +11,44 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             Item currentItem = items[i];
             ItemWrapper itemWrapper = new ItemWrapper(currentItem);
+
+            /*
+                if(BRIE){
+                    if (quality < 50)
+                        increase quality
+                    decrease sellin
+                    if sellin < 0 && quality < 50
+                        increase quality
+                }
+
+                if (SULFURAS)
+                {}
+
+                if (BACKSTAGE){
+                    if (quality < 50)
+                        increase quality
+                        increase hype
+                    decrease sellin
+                    if (sellDatePassed)
+                        quality 0
+                }
+
+                if (STANDARD) {
+                    if (quality > 0)
+                        decrease quality
+                    decrease sellin
+                    if (sellin < 0)
+                        decrease quality
+                }
+
+             */
+            if (itemWrapper.itemIs("Sulfuras, Hand of Ragnaros")){
+                continue;
+            }
             if (itemWrapper.itemIsNot("Aged Brie")
                 && itemWrapper.itemIsNot("Backstage passes to a TAFKAL80ETC concert")) {
                 if (itemWrapper.isAboveMinimumQuality()) {
-                    if (itemWrapper.itemIsNot("Sulfuras, Hand of Ragnaros")) {
-                        itemWrapper.decreaseQualityValue();
-                    }
+                    itemWrapper.decreaseQualityValue();
                 }
             } else {
                 if (itemWrapper.isBelowMaximumQuality()) {
@@ -28,17 +60,13 @@ class GildedRose {
                 }
             }
 
-            if (itemWrapper.itemIsNot("Sulfuras, Hand of Ragnaros")) {
-                itemWrapper.decreaseSellInValue();
-            }
+            itemWrapper.decreaseSellInValue();
 
             if (itemWrapper.isSellInDatePassed()) {
                 if (itemWrapper.itemIsNot("Aged Brie")) {
                     if (itemWrapper.itemIsNot("Backstage passes to a TAFKAL80ETC concert")) {
                         if (itemWrapper.isAboveMinimumQuality()) {
-                            if (itemWrapper.itemIsNot("Sulfuras, Hand of Ragnaros")) {
-                                itemWrapper.decreaseQualityValue();
-                            }
+                            itemWrapper.decreaseQualityValue();
                         }
                     } else {
                         itemWrapper.dropQuality();

55a233b1ac84f954f99ecb955fba572207a5cda0
aclaudel <arnaud.claudel@codurance.com>
2019-11-15 14:34:28 +0000

    sellInValue decrease / sulfuras check extracted

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,7 +12,7 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (isNotSulfuras(items[i])) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -36,15 +36,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (isNotSulfuras(items[i])) {
+                decreaseSellInValue(i);
             }
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (isNotSulfuras(items[i])) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
@@ -58,5 +58,13 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private void decreaseSellInValue(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+    }
+
+    private boolean isNotSulfuras(Item item) {
+        return !item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void sellIn_value_decreases_when_not_Sulfuras() {
+        Item item = new Item("shoes of flying", 10, 10);
+        new GildedRose(new Item[]{item}).updateQuality();
+        assertEquals(9, item.quality);
+    }
+
+    @Test
+    public void sellIn_doesnt_decrease_with_Sulfuras() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 10, 10);
+        new GildedRose(new Item[]{item}).updateQuality();
+        assertEquals(10, item.quality);
     }
 
 }

8120a4a3e9e6fc550d6a0eec56c8c40ca279ca0f
aclaudel <arnaud.claudel@codurance.com>
2019-11-15 16:06:58 +0000

    quality behavior when sellin is negative

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -12,13 +12,13 @@ public class TexttestFixture {
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 1),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 7;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
@@ -30,7 +30,7 @@ public class TexttestFixture {
                 System.out.println(item);
             }
             System.out.println();
-            app.updateQuality();
+            app.updateItems();
         }
     }
 
